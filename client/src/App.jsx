423f299ebdce6ba983eb9d95c8c12fdba8d2a338
shefket-mustafa <miracle_1@abv.bg>
2025-03-15 14:02:50 +0200

    Some components added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
-
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import './App.css'
 
 function App() {
   
 
   return (
     <div id="box">
+    <Header />
     
-    
+    <main id="main-content">
+    <Home />
+    </main>
 
 
     </div>
